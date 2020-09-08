f0ef79fb534fb3b158804a7bf3dc74d9d8625b6d
Nancy Noyes <nnoyes@comcast.net>
2020-09-08 20:49:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(string)
-  if string == "Hi!"
+  if string == "Hi"
     "HUH?! SPEAK UP, SONNY!"
 
   end
