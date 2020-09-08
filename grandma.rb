c53d9f70c212ed125e24fd012bb435315f982435
Nancy Noyes <nnoyes@comcast.net>
2020-09-08 21:02:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(x)
-  if x == "Hi"
+  if x == "Hi!"
     "HUH?! SPEAK UP, SONNY!"
     
     y = "Nancy"
